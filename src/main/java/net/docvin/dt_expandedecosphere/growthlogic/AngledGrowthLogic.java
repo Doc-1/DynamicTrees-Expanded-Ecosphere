310786f09e137cd440accb86b6bbad773c8e4c42
_Doc <ldoc256@gmail.com>
2025-01-21 10:30:17 -0500

    Added commits to not forget how the logic works

--- src/main/java/net/docvin/dt_expandedecosphere/growthlogic/AngledGrowthLogic.java
+++ src/main/java/net/docvin/dt_expandedecosphere/growthlogic/AngledGrowthLogic.java
@@ -32,20 +32,20 @@ public class AngledGrowthLogic extends GrowthLogicKit {
         probMap[0] = 0; //Never go down.
         probMap[1] = 2;
         if (signal.isInTrunk()) {
-            if (signal.numSteps % 3 == 0) {
+            if (signal.numSteps % 3 == 0) { //Makes sure branches start growing every 3 blocks high from the last branch. This prevents branches from overlapping
                 int dist = (context.pos().getY() - rootPos.getY());
                 int i = getDirVariation(context.level(), rootPos, dist, 4);
                 probMap[1] = 1;
                 probMap[i] = 1;
             }
-        } else if (signal.numTurns == 1) {
+        } else if (signal.numTurns == 1) { //Lets the branch grow out one block. This is so the trunk does not prevent the branch from growing upwards.
             boolean flag = (signal.numSteps % 3) == 2;
             probMap[originDir.get3DDataValue()] = flag ? 0 : 1;
             probMap[1] = flag ? 1 : 0;
-        } else if (signal.numTurns >= 2) {
-            int offset = (signal.numSteps - signal.numTurns - 1);
+        } else if (signal.numTurns >= 2) { //After it makes two turns it is ready to start the staircase pattern
+            int offset = (signal.numSteps - signal.numTurns - 1); //Gets the offset to match the original dist when it first left the trunk
             int i = getDirVariation(context.level(), rootPos, offset, 4);
-            boolean flag = (signal.numTurns % 2) == 0;
+            boolean flag = (signal.numTurns % 2) == 0; //if the turn is even then go the cardinal direction set by i, otherwise go up.
             probMap[1] = flag ? 0 : 1;
             probMap[i] = flag ? 1 : 0;
         }
