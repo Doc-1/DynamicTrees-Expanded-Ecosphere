5ba6b765eba482e35eb0de5b50d6e1cf5d430563
_Doc <ldoc256@gmail.com>
2025-07-19 01:53:37 -0400

    Added trees for flooded biomes and flowering pantanal

---
+++ src/main/java/net/docvin/dt_expandedecosphere/growthlogic/root/RootGrowthLogic.java
@@ -0,0 +1,11 @@
+package net.docvin.dt_expandedecosphere.growthlogic.root;
+
+import com.ferreusveritas.dynamictrees.growthlogic.GrowthLogicKit;
+import net.minecraft.resources.ResourceLocation;
+
+public class RootGrowthLogic extends GrowthLogicKit {
+
+    public RootGrowthLogic(ResourceLocation registryName) {
+        super(registryName);
+    }
+}
